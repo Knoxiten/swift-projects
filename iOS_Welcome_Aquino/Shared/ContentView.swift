a2fb96d190b118e10a21a7e65fa0c34f57a7c1f3
Adrian Aquino <aaquino709.ga@gmail.com>
2023-03-01 21:02:11 -0500

    Add files via upload

---
+++ iOS_Welcome_Aquino/Shared/ContentView.swift
@@ -0,0 +1,18 @@
+//  On my honor, I have neither received nor given any unauthorized assistance on Assignment 2." sign with your name here A.A.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to iOS Application Development!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+            
+    }
+}
---
+++ iOS_Welcome_Aquino/Shared/iOS_Welcome_AquinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Welcome_AquinoApp.swift
+//  Shared
+//
+//  Created by Adrian Aquino on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Welcome_AquinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
